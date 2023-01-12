2841b13ccdf7ec0c7d86059a60bdede814177a6f
ARANCIBIA Pablo (pablo.arancibia) <pablo.arancibia@chaco.gob.ar>
2023-01-12 20:28:47 -0300

    cambios retirar , actualizacion package.json

--- frontend/ARAN/src/app/ordentrabajo/retirar/retirar.page.ts
+++ frontend/ARAN/src/app/ordentrabajo/retirar/retirar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-retirar',
@@ -10,6 +12,10 @@ export class RetirarPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNumeroTarjeta(){
+    
   }
 
 }
