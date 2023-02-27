6f6f2b155923cc8bbff128db7305cebfc82e8a10
ARANCIBIA Pablo (pablo.arancibia) <pablo.arancibia@chaco.gob.ar>
2023-02-27 18:42:11 -0300

    delete node_modules with material install

--- frontend/ARAN/src/app/app.module.ts
+++ frontend/ARAN/src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { LOCALE_ID,NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import localEs from '@angular/common/locales/es-AR'
 import { DatePipe, registerLocaleData } from '@angular/common';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 registerLocaleData(localEs,'es-AR')
-
 @NgModule({
   declarations: [AppComponent, SidenavComponent,],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-    HttpClientModule, NgxDatatableModule],
+    HttpClientModule, NgxDatatableModule, BrowserAnimationsModule],
   providers: [{ 
     provide:  RouteReuseStrategy, 
     useClass: IonicRouteStrategy, 
